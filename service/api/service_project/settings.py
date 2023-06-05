bd64dcdf567cc43c187788d1621753d067c4cc0a
WilbertX <cm690buds@outlook.com>
2023-06-05 18:22:09 -0400

    first commit with modules

--- service/api/service_project/settings.py
+++ service/api/service_project/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'service_rest',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +60,9 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost:8090",
+    "http://localhost:8100",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.IntegerField(unique=True)
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=20)
+    vin = models.CharField(max_length=17, unique=True)
+    customer = models.CharField(max_length=100)
+    technician = models.ForeignKey(Technician, on_delete=models.CASCADE)
+
+    def get_api_url(self):
+        return reverse("api_show_appointment", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.customer} - {self.date_time}"
+
+    class Meta:
+        ordering = ("date_time", "reason", "status", "vin", "customer", "technician")
