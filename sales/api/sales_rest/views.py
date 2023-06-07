1a325f407dd9080b5ba80c8fbef2400983b9b492
Matt Hoadley <matt.hoadley1990@gmail.com>
2023-06-07 22:19:32 +0800

    edits sales list create view

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -33,11 +33,31 @@ class SaleListEncoder(ModelEncoder):
     model = Sale
     properties = ["price", "automobile", "salesperson", "customer", "sale_id"]
 
-    encoders = {
-        "automobile": AutomobileVOEncoder(),
-        "salesperson": SalespersonListEncoder(),
-        "customer": CustomerListEncoder(),
-    }
+    # encoders = {
+    #     "automobile": AutomobileVOEncoder(),
+    #     "salesperson": SalespersonListEncoder(),
+    #     "customer": CustomerListEncoder(),
+    # }
+
+    def default(self, o):
+        if isinstance(self, Salesperson):
+            return {
+                "first_name": o.first_name,
+                "last_name": o.last_name,
+                "employee_id": o.employee_id,
+            }
+        if isinstance(self, Customer):
+            return {
+                "first_name": o.first_name,
+                "last_name": o.last_name,
+                "customer_id": o.customer_id,
+            }
+        if isinstance(self, AutomobileVO):
+            return {
+                "vin": o.vin,
+                "sold": o.sold,
+            }
+        return super().default(o)
 
 
 @require_http_methods(["GET", "POST"])
@@ -137,10 +157,10 @@ def api_delete_customer(request, pk):
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_sales(request, sale_id=None):
+def api_list_sales(request, vin=None):
     if request.method == "GET":
-        if sale_id is not None:
-            sales = Sale.objects.filter(sale=sale_id)
+        if vin is not None:
+            sales = Sale.objects.filter(vin=vin)
         else:
             sales = Sale.objects.all()
         return JsonResponse(
@@ -150,7 +170,7 @@ def api_list_sales(request, sale_id=None):
     else:
         content = json.loads(request.body)
         try:
-            sale = Sale.objects.get(sale_id=content["sale_id"])
+            sale = Sale.objects.get(vin=content["vin"])
             return JsonResponse(
                 sale,
                 encoder=SaleListEncoder,
