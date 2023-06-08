1d48fe02f22f7635a6e0e682b45ab9810f0a1852
WilbertX <cm690buds@outlook.com>
2023-06-08 11:55:15 -0400

    InventoryList.js

---
+++ ghi/app/src/AutomobileList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function AutomobileList(props) {
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Color</th>
+            <th>Year</th>
+            <th>VIN</th>
+            <th>Sold</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.automobiles.map((automobile) => (
+            <tr key={automobile.id}>
+              <td>{automobile.color}</td>
+              <td>{automobile.year}</td>
+              <td>{automobile.vin}</td>
+              <td>{automobile.sold}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  export default AutomobileList;
