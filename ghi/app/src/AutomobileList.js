c3d4a2d241a65c919ac9a28b8a30466d33289c9d
WilbertX <cm690buds@outlook.com>
2023-06-08 15:57:38 -0400

    1st form done

--- ghi/app/src/AutomobileList.js
+++ ghi/app/src/AutomobileList.js
@@ -1,28 +1,27 @@
 import React from 'react'
 
 function AutomobileList(props) {
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Color</th>
-            <th>Year</th>
-            <th>VIN</th>
-            <th>Sold</th>
-            <th>Model</th>
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Color</th>
+          <th>Year</th>
+          <th>VIN</th>
+          <th>Sold</th>
+          <th>Model</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.automobiles.map((automobile) => (
+          <tr key={automobile.id}>
+            <td>{automobile.color}</td>
+            <td>{automobile.year}</td>
+            <td>{automobile.vin}</td>
+            <td>{automobile.sold}</td>
+            <td>{automobile.model.name}</td>
           </tr>
-        </thead>
-        <tbody>
-          {props.automobiles.map((automobile) => (
-            <tr key={automobile.id}>
-              <td>{automobile.color}</td>
-              <td>{automobile.year}</td>
-              <td>{automobile.vin}</td>
-              <td>{automobile.sold}</td>
-              <td>{automobile.model.name}</td>
-            </tr>
-          )
-        })}
+        ))}
       </tbody>
     </table>
   );
