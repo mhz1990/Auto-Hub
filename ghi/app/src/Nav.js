c4f6a18eeef591a306df33318fae308e2d2acc8b
Matt Hoadley <matt.hoadley1990@gmail.com>
2023-06-14 00:36:26 +0800

    initial commit changing title of project and logo

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/">Auto Hub</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
